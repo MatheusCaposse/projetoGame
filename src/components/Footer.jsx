2e7aa66688f077cbcbe3e9090791c8ecbe97664d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 11:17:07 -0300

    criando a estilizacao do Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { FooterContainer } from '../style/FooterStyled'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>@2025</h1>
-    </div>
+    <FooterContainer>
+      <div className='contato'>
+        <h3>Contato</h3>
+        <p>Email: matheus.caposse02@gmail.com</p>
+        <p>Telefone: (11)99528-3425</p>
+      </div>
+
+      <div className='direitos'>
+        <p>&copy; 2025 - Todos os Direitos reservados</p>
+      </div>
+
+      <div className='redes-sociais'>
+        <h3>Redes Sociais: </h3>
+        <ul>
+          <li><a href='#' >Facebook</a></li>
+          <li><a href='#' >Instagram</a></li>
+          <li><a href='#' >Twitter</a></li>
+        </ul>
+      </div>
+    </FooterContainer>
+    
   )
 }
 
---
+++ src/style/FooterStyled.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+    background: blue;
+    color:black;
+    padding:20px;
+    margin-top:auto;
+
+    display:flex;
+    justify-content:space-around;
+    max-width:1200px;
+    margin:auto;
+
+    h3{
+        margin-bottom:10px;
+    }
+    ul{
+        list-style:none;
+        padding:0px;
+    }
+    li{
+        margin-bottom:5px;
+    }
+    a{
+        color:gray;
+        text-decoration:none;
+    }
+
+`
